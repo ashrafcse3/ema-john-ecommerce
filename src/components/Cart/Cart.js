ad01dbf014cd62b7af0a6a8c4264a4987c52976f
Ashraful Alam <ashrafcse3@gmail.com>
2022-09-27 12:49:30 +0100

    cart calculation added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,17 +5,26 @@ import './Cart.css';
 
 const Cart = ({ cart }) => {
     console.log(cart);
+    let total = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = (total * 10 / 100).toFixed(2);
+    const grandTotal = total + totalShipping + parseInt(tax);
+
     return (
         <div className='cart'>
             <h4 className='cart-summary'>Order summary</h4>
             <div className='cart-all-info'>
                 <div className='cart-info'>
                     <p>Selected Items: {cart.length}</p>
-                    <p>Total Price: $1140</p>
-                    <p>Total Shipping Charge: $5</p>
-                    <p>Tax: $114</p>
+                    <p>Total Price: ${total}</p>
+                    <p>Total Shipping Charge: ${totalShipping}</p>
+                    <p>Tax: ${tax}</p>
                 </div>
-                <p className='grand-total'>Grand Total: $1559</p>
+                <p className='grand-total'>Grand Total: ${grandTotal}</p>
             </div>
             <div className='cart-buttons'>
                 <button className='clear-cart-button'>
